6d3bd2326a893a89b01da4df1c6e5e4fe06a57c6
Filip3ra <filipimacielrj@gmail.com>
2024-02-27 15:55:30 -0300

    Descrição tp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include "header.hpp"
 using namespace std;
 
+/* Makespan: reduzir ao máximo o tempo total necessário para
+completar todas as tarefas em um ambiente de produção.
+O makespan é o tempo total decorrido desde o início até a
+conclusão de todas as tarefas em um determinado cronograma de produção.
+
+No problema Job Shop, há um conjunto de trabalhos que precisam ser
+processados em diferentes máquinas, com cada trabalho consistindo
+em uma sequência específica de operações que devem ser executadas
+em ordem específica. Cada operação de um trabalho deve ser processada
+em uma máquina particular, e cada máquina só pode processar uma
+operação de cada vez.
+
+*/
+
 int main()
 {
   cout << "Hello World!";
