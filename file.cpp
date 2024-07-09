6e1de1b3da7748469e2f99eb54401fcb079e3e14
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2024-07-09 16:13:24 -0300

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -15,7 +15,7 @@ JobShopScheduler::JobShopScheduler(const string &file_name)
 
   gifflerThompson(dag);
 
-  vector<unsigned> starts = calcStartTimes(dag);
+
 }
 
 // Função para ler o arquivo e extrair as informações
